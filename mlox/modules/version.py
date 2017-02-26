b03923b73492e2af8af10e7c8ac9e254a2505f9a
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-02-26 08:14:56 -0500

    mlox_gui:  Move to a separate module
    Also create a version module.

---
+++ mlox/modules/version.py
@@ -0,0 +1,35 @@
+
+import os
+import sys
+import re
+import locale
+from modules.loadOrder import base_file
+
+Version = "0.61"
+
+re_base_version = re.compile(r'^\[version\s+([^\]]*)\]', re.IGNORECASE)
+full_version = ""
+
+def version_info():
+    try:
+        import wx.__version__ as wx_version
+    except:
+        wx_version = "Not Installed"
+    return "%s (%s/%s)\nPython Version: %s\nwxPython Version: %s\n" % (full_version(), locale.getdefaultlocale()[0], locale.getpreferredencoding(), sys.version[:3], wx_version)
+
+def get_mlox_base_version():
+    try:
+        base = open(base_file, 'r')
+    except IOError:
+        logging.error("Unable to get rules file version from:  {0}".format(base_file))
+        return("(Not Found)")
+    for line in base:
+        m = re_base_version.match(line)
+        if m:
+            base.close()
+            return(m.group(1))
+    base.close()
+    return("(Not Found)")
+
+def full_version():
+    return "%s %s [mlox-base %s]" % (os.path.basename(sys.argv[0]), Version, get_mlox_base_version())
