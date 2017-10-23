0d97a9ebb572667ea56baef66d2b640b1d6c87a4
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-10-23 02:29:58 -0400

    Make Printing the version information more generic

--- mlox/modules/version.py
+++ mlox/modules/version.py
@@ -5,11 +5,19 @@ import locale
 Version = "0.62"
 
 def version_info():
+    "Returns a human readable multi-line string containting the program's version information"
     try:
         import wx.__version__ as wx_version
+        gui_version = "wxPython Version: {0}".format(wx_version)
     except:
-        wx_version = "Not Installed"
-    return "%s (%s/%s)\nPython Version: %s\nwxPython Version: %s\n" % (full_version(), locale.getdefaultlocale()[0], locale.getpreferredencoding(), sys.version[:3], wx_version)
+        gui_version = "wxPython Not Installed!"
+    return "{program} ({locale}/{encoding})\nPython Version: {python_version}\n{gui_version}\n".format(
+        program=full_version(),
+        locale=locale.getdefaultlocale()[0],
+        encoding=locale.getpreferredencoding(),
+        python_version=sys.version[:3],
+        gui_version=gui_version
+    )
 
 def full_version():
     return "{0} {1}".format(os.path.basename(sys.argv[0]), Version)
