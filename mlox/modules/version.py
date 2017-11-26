eadac0c270dd01e9e1dedade2c735c92dd0c2579
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-11-26 09:05:59 -0500

    version.py:  Added more version information
    We now tell the user what libraries are installed.

--- mlox/modules/version.py
+++ mlox/modules/version.py
@@ -5,19 +5,34 @@ import locale
 Version = "0.62"
 
 def version_info():
-    "Returns a human readable multi-line string containting the program's version information"
-    try:
-        from PyQt5.QtCore import QT_VERSION_STR
-        gui_version = "PyQt5 Version: {0}".format(QT_VERSION_STR)
-    except:
-        gui_version = "PyQt5 Not Installed!"
-    return "{program} ({locale}/{encoding})\nPython Version: {python_version}\n{gui_version}\n".format(
+    """
+    :return: a human readable multi-line string containing the program's version information
+    """
+    output = "{program} ({locale}/{encoding})\n".format(
         program=full_version(),
         locale=locale.getdefaultlocale()[0],
         encoding=locale.getpreferredencoding(),
-        python_version=sys.version[:3],
-        gui_version=gui_version
+        python_version=sys.version[:3]
     )
+    try:
+        from PyQt5.QtCore import QT_VERSION_STR
+        output += "PyQt5 Version: {0}\n".format(QT_VERSION_STR)
+    except ImportError:
+        output += "PyQt5 Not Installed!\n"
+
+    try:
+        from appdirs import __version_info__ as appdirs_version
+        output += "appdirs Version: {0}\n".format(".".join(list(map(str,appdirs_version))))
+    except ImportError:
+        output += "appdirs Not Installed!\n"
+
+    try:
+        import libarchive
+        output += "libarchive Installed.\n"
+    except ImportError:
+        output += "libarchive Not Installed!\n"
+
+    return output
 
 def full_version():
     return "{0} {1}".format(os.path.basename(sys.argv[0]), Version)
